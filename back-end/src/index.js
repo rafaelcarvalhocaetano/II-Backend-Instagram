a165ccb445adbf1b7f88834d87e641b0bdd6e9d5
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2019-06-17 11:37:36 -0300

    Backend

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -11,12 +11,10 @@ const io = require('socket.io')(server);
 mongoose.connect('mongodb+srv://backend:10Sistem@s@cluster0-ejlyp.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true
 });
-
 app.use((req, res, next) => {
   req.io = io;
   next();
 })
-
 app.use(cors());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 app.use(require('./routes/routes'));
