d677a86bc0d32bf7d6989594f5868d3a8b4181fc
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2019-06-17 17:06:39 -0300

    Finish

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -6,46 +6,32 @@ const fs = require('fs');
 module.exports = {
    
   async index(req, res) {
-    try {
-      const posts = await Post.find().sort('-createdAt')
-      .limit(5).exec((e, msg) => {
-        console.log(' s ', e);
-        console.log(' msg ', msg);
-      });
-      return res.json(posts);
-    } catch (e) {
-      console.log(' 4 ', e);
-      
-    }   
+    const posts = await Post.find().sort('-createdAt');
+    return res.json(posts);
   },
 
   async store(req, res) {
-    try {
-      const { author, place, description, hashtags } = req.body;
-      const { filename: image } = req.file;
-      const [name] = image.split('.');
-      const fileName = `${name}.jpg`;
+    const { author, place, description, hashtags } = req.body;
+    const { filename: image } = req.file;
+    const [name] = image.split('.');
+    const fileName = `${name}.jpg`;
 
-      // tratando a imagem com sharp
-      await sharp(req.file.path)
-        .resize(500)
-        .jpeg({ quality: 70 })
-        .toFile(path.resolve(req.file.destination, 'resized', fileName));
-      //delete image with file-system
-      fs.unlinkSync(req.file.path);
+    // tratando a imagem com sharp
+    await sharp(req.file.path)
+      .resize(500)
+      .jpeg({ quality: 70 })
+      .toFile(path.resolve(req.file.destination, 'resized', fileName));
+    //delete image with file-system
+    fs.unlinkSync(req.file.path);
 
-      const post = await Post.create({
-        author,
-        place,
-        description,
-        hashtags,
-        image: fileName
-      }, (e) => console.log(' erro postcontroller ', e.error));
-      req.io.emit('post', post);
-      return res.json(post);
-    } catch (error) {
-      console.log(' error ', error)
-    }
-   
+    const post = await Post.create({
+      author,
+      place,
+      description,
+      hashtags,
+      image: fileName
+    }, (e) => console.log(' erro postcontroller ', e.error));
+    req.io.emit('post', post);
+    return res.json(post);
   }
 };
--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
+const allowCors = require('./cors');
 
-const app = express();
+const app = express ();
 
-const conectionDB = process.env.MONGO_URL || "mongodb+srv://backend:10Sistem@s@cluster0-ejlyp.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.Promise = global.Promise
+module.exports = mongoose.connect('mongodb+srv://backend:10Sistem@s@cluster0-ejlyp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+
+app.use(allowCors);
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-mongoose.connect( conectionDB,  { useNewUrlParser: true } );
 app.use((req, res, next) => {
   debugger
   req.io = io;
