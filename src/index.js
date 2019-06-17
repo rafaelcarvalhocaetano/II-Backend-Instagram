6a6f53216c87cad7df45b2ef5c8d0276f1e1149b
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2019-06-17 14:40:20 -0300

    Finish

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ const cors = require('cors');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000
-
-const conectionDB = process.env.MONGO_URL || 'mongodb+srv://backend:10Sistem@s@cluster0-ejlyp.mongodb.net/test?retryWrites=true&w=majority'
+const conectionDB = process.env.MONGO_URL || "mongodb+srv://backend:10Sistem@s@cluster0-ejlyp.mongodb.net/test?retryWrites=true&w=majority";
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
@@ -22,4 +20,4 @@ app.use(cors());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 app.use(require('./routes/routes'));
 
-server.listen(PORT);
+server.listen(process.env.PORT || 3300);
