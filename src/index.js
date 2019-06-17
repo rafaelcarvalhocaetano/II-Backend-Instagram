f720055017ea8bde8278f838a3e3463a57d1fbaa
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2019-06-17 13:31:13 -0300

    Updating code backend

--- src/index.js
+++ src/index.js
@@ -8,9 +8,12 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-mongoose.connect('mongodb+srv://backend:10Sistem@s@cluster0-ejlyp.mongodb.net/test?retryWrites=true&w=majority', {
-  useNewUrlParser: true
-});
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true
+  }
+);
 app.use((req, res, next) => {
   req.io = io;
   next();
