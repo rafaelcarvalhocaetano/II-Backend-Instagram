fcaf69397c2e7924c6d756704f0263b6ab39808f
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2019-06-17 16:14:10 -0300

    fisnasd

--- src/index.js
+++ src/index.js
@@ -14,13 +14,13 @@ const io = require('socket.io')(server);
 mongoose.connect( conectionDB,  { useNewUrlParser: true } );
 app.use((req, res, next) => {
   req.io = io;
-  res.setHeader('Access-Control-Allow-Origin', 'https://backend-instagram.herokuapp.com/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://backend-instagram.herokuapp.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 })
-app.use(cors());
+app.use(cors({ origin: 'https://backend-instagram.herokuapp.com'}));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 app.use(require('./routes/routes'));
 
